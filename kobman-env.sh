e528a6c416c04f837fcd0c3c3e3b2e6a6873d203
asa1997 <arunsureshampadath@gmail.com>
2020-10-14 15:31:09 +0530

    Updated

--- kobman-env.sh
+++ kobman-env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-__kobman_install_env
+function __kobman_install_env
 {
     mkdir $HOME/env1_folder
     echo "created"
 }
 
-__kobman_uninstall_env
+function __kobman_uninstall_env
 {
     [[ -d $HOME/env1_folder ]] && rm -rf $HOME/env1_folder
     echo "removed"
